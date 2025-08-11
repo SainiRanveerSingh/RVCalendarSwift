ad3950e9b78408705b0b68dcf5725b472c8fbb82
Ranveer Singh Saini <sainiranveersingh@gmail.com>
2025-08-12 00:26:21 +0530

    • For temporary time commented the delegate assigning code

--- Sources/RVCalendar/RVCalendar/RVCalendar.swift
+++ Sources/RVCalendar/RVCalendar/RVCalendar.swift
@@ -56,10 +56,10 @@ public class RVCalendar: UIView {
         DispatchQueue.main.asyncAfter(deadline: .now() + 2.0) { [weak self] in
             guard let self = self else { return }
             if self.calendarWeekView != nil {
-                self.calendarWeekView.weekViewCalendarDelegate = self
+                //self.calendarWeekView.weekViewCalendarDelegate = self
             }
             if self.calendarMonthView != nil {
-                self.calendarMonthView.monthViewCalendarDelegate = self
+                //self.calendarMonthView.monthViewCalendarDelegate = self
             }
         }
         self.calendarMonthView.clipsToBounds = true
